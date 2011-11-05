0b1459e075249f7249493c5739e2f5b79c994c29
Leo.Shi <mleoshi@gmail.com>
2011-11-06 02:09:30 +0800

    added all source files

---
+++ OpenSinaApi/OpenSinaApi-04-18/Oauth4Web/Sample.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Sample.aspx.cs" Inherits="Oauth4Web._Default" validateRequest="false"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>无标题页</title>
+    <style type="text/css">
+        #Select1
+        {
+            width: 126px;
+        }
+        #MethodsSelector
+        {
+            width: 119px;
+        }
+        #Text1
+        {
+            height: 233px;
+            width: 1216px;
+        }
+        #resultText
+        {
+            height: 186px;
+            width: 1215px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:Button ID="sinaButton" runat="server" onclick="sinaButton_Click" 
+        Text="连接sina" />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Methods:"></asp:Label>
+&nbsp;
+    <asp:DropDownList ID="DropDownList" runat="server" Height="20px" Width="142px">
+        <asp:ListItem Value="0">public_timeline</asp:ListItem>
+        <asp:ListItem Value="1">friends_timeline</asp:ListItem>
+        <asp:ListItem Value="2">user_timeline</asp:ListItem>
+        <asp:ListItem Value="3">mentions</asp:ListItem>
+        <asp:ListItem Value="4">comments_timeline</asp:ListItem>
+        <asp:ListItem Value="5">comments_by_me</asp:ListItem>
+        <asp:ListItem Value="6">statuses/followers</asp:ListItem>
+    </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="invoke" runat="server" onclick="invoke_Click" Text="Invoke" />
+    <br />
+    <p>
+        <asp:TextBox ID="resultText" runat="server" Height="247px" TextMode="MultiLine" 
+            Width="1205px"></asp:TextBox>
+    </p>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="status:"></asp:Label>
+    <asp:TextBox ID="statusText" runat="server" Width="210px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="update" runat="server" onclick="update_Click" Text="Update" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="Label3" runat="server" Text="filepath:"></asp:Label>
+&nbsp;<asp:TextBox ID="filepath" runat="server" Width="202px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="upload" runat="server" onclick="upload_Click" Text="Upload" />
+    <br />
+    <br />
+    <asp:TextBox ID="resultTextBox2" runat="server" Height="276px" 
+        TextMode="MultiLine" Width="1207px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ OpenSinaApi/OpenSinaApi-06-30/Oauth4Web/Sample.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using LeoShi.Soft.OpenSinaAPI;
+
+
+namespace Oauth4Web
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                var httpRequest = HttpRequestFactory.CreateHttpRequest(Method.GET) as HttpGet;
+                if (Request["oauth_verifier"] != null)
+                {
+                    httpRequest.Token = Session["oauth_token"].ToString();
+                    httpRequest.TokenSecret = Session["oauth_token_secret"].ToString();
+                    httpRequest.Verifier = Request["oauth_verifier"];
+                    httpRequest.GetAccessToken();
+                    Session["oauth_token"] = httpRequest.Token;
+                    Session["oauth_token_secret"] = httpRequest.TokenSecret;
+                    Response.Redirect("/Sample.aspx");
+                    
+                }
+                if (Session["oauth_token"]!=null)
+                    this.sinaButton.Text = "连接成功";
+            }         
+        }
+
+        protected void sinaButton_Click(object sender, EventArgs e)
+        {
+            var httpRequest = HttpRequestFactory.CreateHttpRequest(Method.GET) as HttpGet;
+            httpRequest.GetRequestToken();
+            string url = httpRequest.GetAuthorizationUrl();
+            Session["oauth_token"] = httpRequest.Token;
+            Session["oauth_token_secret"] = httpRequest.TokenSecret;
+            Response.Redirect(url + "&oauth_callback=http://localhost:3668/Sample.aspx");
+        }
+
+        protected void invoke_Click(object sender, EventArgs e)
+        {
+            var httpRequest = HttpRequestFactory.CreateHttpRequest(Method.GET);
+            httpRequest.Token = Session["oauth_token"].ToString();
+            httpRequest.TokenSecret = Session["oauth_token_secret"].ToString();
+            httpRequest.UserRemoteIP = Request.UserHostAddress;
+            string basic = "http://api.t.sina.com.cn/";
+            string url = "";
+            switch (this.DropDownList.SelectedIndex)
+            {
+                case 0:
+                    url = basic + "statuses/public_timeline.xml";
+                    break;
+                case 1:
+                    url = basic + "statuses/friends_timeline.xml";
+                    break;
+                case 2:
+                    url = basic + "statuses/user_timeline.xml";
+                    break;
+                case 3:
+                    url = basic + "statuses/mentions.xml";
+                    break;
+                case 4:
+                    url = basic + "statuses/comments_timeline.xml";
+                    break;
+                case 5:
+                    url = basic + "statuses/comments_by_me.xml";
+                    break;
+                case 6:
+                    url = basic + "statuses/followers.xml";
+                    break;
+                default:
+                    break;
+            }
+            this.resultText.Text = httpRequest.Request(url, String.Empty);
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            var httpRequest = HttpRequestFactory.CreateHttpRequest(Method.POST);
+            httpRequest.Token = Session["oauth_token"].ToString();
+            httpRequest.TokenSecret = Session["oauth_token_secret"].ToString();
+            httpRequest.UserRemoteIP = Request.UserHostAddress;
+            var url = "http://api.t.sina.com.cn/statuses/update.xml?";
+            resultTextBox2.Text = httpRequest.Request(url, "status=" + HttpUtility.UrlEncode(statusText.Text));
+        }
+
+        protected void upload_Click(object sender, EventArgs e)
+        {
+            var httpRequest = HttpRequestFactory.CreateHttpRequest(Method.POST) as HttpPost;
+            httpRequest.Token = Session["oauth_token"].ToString();
+            httpRequest.TokenSecret = Session["oauth_token_secret"].ToString();
+            httpRequest.UserRemoteIP = Request.UserHostAddress;
+            string url = "http://api.t.sina.com.cn/statuses/upload.xml?";
+            this.resultTextBox2.Text = httpRequest.RequestWithPicture(url, "status=" + HttpUtility.UrlEncode(statusText.Text), filepath.Text);
+        }
+    }
+}
